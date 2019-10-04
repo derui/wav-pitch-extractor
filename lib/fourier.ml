99debfebfbc0b991423bde24b4ff4a4d031cdef8
derui <derutakayu@gmail.com>
2019-10-04 10:46:06 +0900

    feat: add FFT by bluestein's algorithm

--- lib/fourier.ml
+++ lib/fourier.ml
@@ -32,6 +32,34 @@ let loop s e ~f =
 
 let to_s {Complex.re; im} = Printf.sprintf "{%f, %f}" re im
 
+let fft_2_power (data : Complex.t array) =
+  let q = 2 in
+  let rec fft' data n stride =
+    if n = 1 then data
+    else
+      let data_fst = fft' (Array.init (n / q) (fun i -> data.(i * q * stride))) (n / q) stride
+      and data_snd =
+        fft' (Array.init (n / q) (fun i -> data.(stride + (i * q * stride)))) (n / q) stride
+      in
+      let ret = Array.make n Complex.zero in
+      loop 0 (n / 2) ~f:(fun i ->
+          let omega =
+            { Complex.re = cos (-2. *. Float.pi *. float_of_int i /. float_of_int n)
+            ; im = sin (-2. *. Float.pi *. float_of_int i /. float_of_int n) }
+          in
+          let t = data_fst.(i) and t2 = data_snd.(i) in
+          ret.(i) <- Complex.add t (Complex.mul t2 omega) ;
+          ret.(i + (n / 2)) <- Complex.sub t (Complex.mul t2 omega)) ;
+      ret
+  in
+  fft' data (Array.length data) 1
+
+let ifft_2_power (data : Complex.t array) =
+  let data' = Array.map Complex.conj data in
+  let data' = fft_2_power data' in
+  let coefficient = {Complex.re = 1. /. (float_of_int @@ Array.length data); im = 0.} in
+  Array.map (Complex.mul coefficient) data'
+
 let fft (data : float array) =
   (* make array that has size to align 2-exponential it *)
   let ( ** ) v e =
@@ -43,84 +71,40 @@ let fft (data : float array) =
     least_exponent_of_2 data_size |> ( ** ) 2 |> Array.make |> fun init -> init Complex.zero
   in
   Array.iteri (fun i v -> size_fixed_data.(i) <- Complex.{re = v; im = 0.}) data ;
-  (* define parameter for FFT *)
-  let q = 2 in
-  (* let n = Array.length size_fixed_data in
-   * let degree = least_exponent_of_2 n in *)
-  let rec fft' data n stride =
-    if n = 1 then data
-    else
-      let data_fst =
-        fft'
-          (Array.init (n / q) (fun i ->
-               Printf.printf "first %d:%s\n"
-                 (i * q * stride)
-                 (to_s size_fixed_data.(i * q * stride)) ;
-               data.(i * q * stride)))
-          (n / q) stride
-      and data_snd =
-        fft'
-          (Array.init (n / q) (fun i ->
-               Printf.printf "second %d:%s\n"
-                 (stride + (i * q * stride))
-                 (to_s size_fixed_data.(stride + (i * q * stride))) ;
-               data.(stride + (i * q * stride))))
-          (n / q) stride
-      in
-      let ret = Array.make n Complex.zero in
-      loop 0 (n / 2) ~f:(fun i ->
-          let omega =
-            { Complex.re = cos (-2. *. Float.pi *. float_of_int i /. float_of_int n)
-            ; im = sin (-2. *. Float.pi *. float_of_int i /. float_of_int n) }
-          in
-          let t = data_fst.(i) and t2 = data_snd.(i) in
-          ret.(i) <- Complex.add t (Complex.mul t2 omega) ;
-          ret.(i + (n / 2)) <- Complex.sub t (Complex.mul t2 omega) ;
-          Printf.printf "omega:%s %d:%s %d:%s\n" (to_s omega) i
-            (to_s data_fst.(i))
-            (i + (n / 2))
-            (to_s data_snd.(i))) ;
-      ret
-  in
-  let result = fft' size_fixed_data (Array.length size_fixed_data) 1 in
+  let result = fft_2_power size_fixed_data in
   Array.sub result 0 data_size
 
-(*   let result = Array.make (Array.length size_fixed_data) Complex.zero in
- * loop 0 degree ~f:(fun deg ->
- *     let pdeg = q ** (degree - (deg + 1)) in
- *     (\* 0 <= p < q ^ (k - 1) *\)
- *     loop 0 pdeg ~f:(fun j0 ->
- *         (\* 0 <= p < q ^ (k - 1) *\)
- *         loop 0 (q ** deg) ~f:(fun j1 ->
- *             let idx = j0 + (j1 * pdeg * q) in
- *             (\* 2-exponent fourier transform. That called butterfly calculation *\)
- *             Printf.printf "j0:j1 %d:%d, index %d, deg %d, total degree %d, pdeg %d, next %d\n%!"
- *               j0 j1 idx deg degree pdeg (idx + pdeg) ;
- *             (\* r = 0 *\)
- *             let w1 = Complex.add size_fixed_data.(idx) size_fixed_data.(idx + pdeg)
- *             (\* r = 1 *\)
- *             and w3 =
- *               Complex.add size_fixed_data.(idx) @@ Complex.neg size_fixed_data.(idx + pdeg)
- *             in
- *             let angle = 2. *. Float.pi *. float_of_int pdeg in
- *             (\* r = 0 *\)
- *             let rotate_factor1 = {Complex.re = 1.; im = 0.}
- *             (\* r = 1 *\)
- *             and rotate_factor2 =
- *               Complex.
- *                 {re = cos (angle *. float_of_int idx); im = sin (angle *. float_of_int idx)}
- *             in
- *             (\* r = 0 *\)
- *             size_fixed_data.(idx) <- Complex.(mul w1 rotate_factor1) ;
- *             (\* r = 1 *\)
- *             size_fixed_data.(idx + pdeg) <- Complex.(mul w3 rotate_factor2)))) ;
- * (\* inverse bit *\)
- * loop 0 (Array.length size_fixed_data) ~f:(fun i ->
- *     let idx = ref i in
- *     let idx_buf = ref 0 in
- *     loop 0 degree ~f:(fun deg ->
- *         let deg = degree - succ deg in
- *         idx_buf := !idx_buf + ((!idx - (!idx / q * q)) * (q ** deg)) ;
- *         idx := !idx / q) ;
- *     result.(i) <- size_fixed_data.(!idx_buf)) ;
- * Array.sub result 0 data_size *)
+let fft_bluestein (data : float array) =
+  (* make array that has size to align 2-exponential it *)
+  let ( ** ) v e =
+    let rec loop ret count = if count = 0 then ret else loop (ret * v) (pred count) in
+    loop 1 e
+  in
+  let data_size = Array.length data in
+  let padded_size = 2 ** least_exponent_of_2 ((data_size * 2) - 1) in
+  let omega i n =
+    { Complex.re = cos (Float.pi *. float_of_int i /. float_of_int n)
+    ; im = sin (Float.pi *. float_of_int i /. float_of_int n) }
+  in
+  let a =
+    Array.init padded_size
+    @@ fun i ->
+    if i < data_size then
+      let v = {Complex.re = data.(i); im = 0.} in
+      let i = omega (-1 * (i ** 2)) data_size in
+      Complex.mul v i
+    else Complex.zero
+  in
+  let b =
+    Array.init padded_size
+    @@ fun i ->
+    if i < data_size then omega (i ** 2) data_size
+    else if data_size <= i && i <= padded_size - data_size then Complex.zero
+    else omega ((data_size - (i - (padded_size - data_size))) ** 2) data_size
+  in
+  let a' = fft_2_power a and b' = fft_2_power b in
+  let r = Array.init padded_size (fun i -> Complex.mul a'.(i) b'.(i)) |> ifft_2_power in
+  Array.init data_size (fun i ->
+      let omega = b.(i) in
+      let r' = r.(i) in
+      Complex.mul omega r')
--- test/fourier_test.ml
+++ test/fourier_test.ml
@@ -1,17 +1,21 @@
 module F = Ocaml_pitch.Fourier
 
-let sin_wave =
+let sin_wave n =
   let radian = Float.pi *. 2. in
-  Array.init 100 (fun v ->
+  Array.init n (fun v ->
       (sin @@ (10. *. radian *. float_of_int v *. 0.01))
       +. (sin @@ (radian *. 20. *. float_of_int v *. 0.01)))
 
 let tests =
   [ Alcotest.test_case "should calculate spectral" `Quick (fun () ->
-        let spectral = F.fourier sin_wave in
+        let spectral = F.fourier @@ sin_wave 100 in
         let spectral = Array.map Complex.norm spectral in
         Alcotest.(check @@ float 0.00001) "data" spectral.(20) 50.)
   ; Alcotest.test_case "should calculate spectral with FFT" `Quick (fun () ->
-        let spectral = F.fft sin_wave |> Array.map Complex.norm
-        and spectral' = F.fourier sin_wave |> Array.map Complex.norm in
+        let spectral = sin_wave 128 |> F.fft |> Array.map Complex.norm
+        and spectral' = sin_wave 128 |> F.fourier |> Array.map Complex.norm in
+        Alcotest.(check @@ array @@ float 0.00001) "FFT" spectral spectral')
+  ; Alcotest.test_case "should calculate spectral with FFT with bluestein" `Quick (fun () ->
+        let spectral = sin_wave 128 |> F.fft_bluestein |> Array.map Complex.norm
+        and spectral' = sin_wave 128 |> F.fourier |> Array.map Complex.norm in
         Alcotest.(check @@ array @@ float 0.00001) "FFT" spectral spectral') ]
