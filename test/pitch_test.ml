7ebb624daf35838ed665e2a163ba3eda266d303f
derui <derutakayu@gmail.com>
2019-11-27 08:19:50 +0900

    fix: pitch test

--- test/pitch_test.ml
+++ test/pitch_test.ml
@@ -6,6 +6,6 @@ let tests =
         let data = P.pitches_with_notation () in
         Alcotest.(check @@ bool) "byte" true List.(mem (P.base_notation, P.base_hz) data));
     Alcotest.test_case "should get pitch of the data" `Quick (fun () ->
-        let data = [| 0.1; 0.; 0.1; 0.5; 0.4 |] in
+        let data = [| 1.1; 1.; 0.1; 0.6; 0.4 |] in
         Alcotest.(check @@ float 0.0001) "pitch" 1.66667 P.(detect_pitch_in ~data ~frame_size:5.));
   ]
